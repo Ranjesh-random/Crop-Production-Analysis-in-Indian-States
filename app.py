e33dab6310f573b90c0c89bb077618dfd2be461c
Ranjesh_random <msa23022@iiitl.ac.in>
2024-08-14 16:45:01 +0530

    updating port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -59,4 +60,5 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
